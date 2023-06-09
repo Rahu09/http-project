2cdab786da3513f1479df5025abec57ae1b7d660
Rahul Tiwari <tiwarirahul0809@gmail.com>
2023-06-09 17:11:03 +0530

    running

--- crawl.js
+++ crawl.js
@@ -1,27 +1,28 @@
-const { error } = require('console')
 const { JSDOM } = require('jsdom')
 
 let normalizeURL = (inputUrl)=>{
-    let url = new URL(inputUrl)
-    let [protocal, hostname, pathname ] = [url.protocol, url.hostname, url.pathname]
-    if(protocal == 'http:') protocal='https:'
-    if(pathname.charAt(pathname.length-1) == '/') pathname=pathname.substring(0,pathname.length-1)
-    return `${protocal}//${hostname}${pathname}`
+  let url = new URL(inputUrl)
+  let [protocal, hostname, pathname ] = [url.protocol, url.hostname, url.pathname]
+  if(protocal == 'http:') protocal='https:'
+  if(pathname.charAt(pathname.length-1) == '/') pathname=pathname.substring(0,pathname.length-1)
+  return `${protocal}//${hostname}${pathname}`
 }
+
 let getURLsFromHTML = (htmlBody, baseURL)=>{
-    let ans = []
-    const dom = new JSDOM(htmlBody);
-    let arr = dom.window.document.querySelectorAll('a')
-    for (let i = 0; i < arr.length; i++) {
-        const ele = arr[i].href;
+  let ans = []
+  const dom = new JSDOM(htmlBody);
+  let arr = dom.window.document.querySelectorAll('a')
 
-        if(ele.length >= baseURL.length && ele.substring(0,baseURL.length) != baseURL) ans[i] = baseURL+ele
-        else if(ele.length >= baseURL.length && ele.substring(0,baseURL.length) == baseURL) ans[i] = ele
-        else ans[i] = baseURL+ele
-
-    }
-    return ans
+  for (let i = 0; i < arr.length; i++) {
+    const ele = arr[i].href;
+    
+    if(ele.length >= baseURL.length && ele.substring(0,baseURL.length) != baseURL) ans[i] = baseURL+ele
+    else if(ele.length >= baseURL.length && ele.substring(0,baseURL.length) == baseURL) ans[i] = ele
+    else ans[i] = baseURL+ele
+    
   }
+  return ans
+}
   
 async function crawlPage (BASE_URL){
   try {
@@ -30,7 +31,11 @@ async function crawlPage (BASE_URL){
     const content = response.headers.get('Content-Type');
     
     if(!status) throw new Error(`${response.status}  error`)
-    if(content != 'text/html') throw new Error(`content type is not text/html error`)
+    else if(content.substring(0,9) != 'text/html') throw new Error(`content type is not text/html error it is${content}`)
+    else{
+      let text = await response.text();
+      console.log(text); 
+    }  
   } catch (error) {
     console.log(error);
   }
@@ -48,7 +53,7 @@ let main = (bl)=>{
     else if(arr.length<1) console.log('cli arg is less than one');
     else{
       console.log('starting web crawling');
-      crawlPage(BASE_URL)
+      crawlPage(num)
     } 
     readline.close();
   });
@@ -56,48 +61,7 @@ let main = (bl)=>{
 
 main()
 
-  // async function crawlPage (BASE_URL){
-  //   let response = await fetch(BASE_URL)
-  //   const status = response.ok;
-  //   const content = response.headers.get('Content-Type');
-    
-  //   if(!status) throw new Error(`${response.status}  error`)
-  //   if(content != 'text/html') throw new Error(`content type is not text/html error`)
-  // }
-  
-  // try {
-  //   crawlPage(BASE_URL);
-  // } catch (error) {
-  //   console.log(error);
-  // }
-
-  
-
-  // function generateKey() {
-  //   const characters = 'ABCDEF0123456789'
-  //   let result = ''
-  //   for (let i = 0; i < 16; i++) {
-  //     result += characters.charAt(Math.floor(Math.random() * characters.length))
-  //   }
-  //   return result
-  // }
-
 module.exports = {
     normalizeURL,
     getURLsFromHTML
 }
-
-// normalizeURL('http://wagslane.dev/path/')
-// getURLsFromHTML(`<html>
-//                     <body>
-//                         <a href="/path/first/hello.txt"><span>Go to Bootq.dev</span></a>
-//                         <p>yooooo<p/>
-//                         <a href=''><span>Go to Boots.dev</span></a>
-//                         <p>yooooo<p/>
-//                         <a href='/path/first'><span>Go to Bootz.dev</span></a>
-//                         <p>yooooo<p/>
-//                         <a href='/path'><span>Go to Bootz.dev</span></a>
-//                     </body>
-//                     </html>`,
-//                     'https://wagslane.dev'
-// )
